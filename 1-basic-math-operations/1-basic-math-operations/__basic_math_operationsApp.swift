99b936bd4031b1c179ab2bbd5bf0915de3737b4a
Jose <jeduramos7001@gmail.com>
2026-03-22 19:57:33 -0700

    Add the base for the 1st app

---
+++ 1-basic-math-operations/1-basic-math-operations/__basic_math_operationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  __basic_math_operationsApp.swift
+//  1-basic-math-operations
+//
+//  Created by Jose Ramos on 22/3/26.
+//
+
+import SwiftUI
+
+@main
+struct __basic_math_operationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
